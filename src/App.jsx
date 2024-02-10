bc9df6a07e6563ead8bafba8a61eda0892748f52
salman170 <salmanansari02120@gmail.com>
2024-02-10 10:25:42 +0530

    done with initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { Blogs } from "./screen/more-links/Blogs";
 import { Outlets } from "./screen/more-links/Outlets";
 import { ContactUs } from "./screen/more-links/ContactUs";
 import { TermsAndConditions } from "./screen/more-links/TermsAndConditions";
-import { PageNotFound } from "./components/PageNotFound";
+import { PageNotFound } from "./screen/page-not-found/PageNotFound";
 
 function App() {
   const { pathname } = useLocation();
@@ -28,8 +28,16 @@ function App() {
       <Route exact path="/used-car-blog" element={<Blogs />} />
       <Route exact path="/used-car-outlets" element={<Outlets />} />
       <Route exact path="/used-car-faqs" element={<FAQs />} />
-      <Route exact path="/contact-preowned-car-dealer" element={<ContactUs />} />
-      <Route exact path="/used-cars-terms-conditions" element={<TermsAndConditions />} />
+      <Route
+        exact
+        path="/contact-preowned-car-dealer"
+        element={<ContactUs />}
+      />
+      <Route
+        exact
+        path="/used-cars-terms-conditions"
+        element={<TermsAndConditions />}
+      />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
