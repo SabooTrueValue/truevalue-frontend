660500adb27e65fb6609fd9b8919f626d66c813c
salman170 <salmanansari02120@gmail.com>
2024-03-08 10:54:52 +0530

    icon updated

--- src/screen/sell-your-car/MainPanel.jsx
+++ src/screen/sell-your-car/MainPanel.jsx
@@ -4,6 +4,35 @@ const MainPanel = ({ selected }) => {
   return (
     <div className="w-full h-full p-2 bg-white lg:p-4 rounded-xl">
       {/* {selected} */}
+      {selected === 0 && dashboard()}
+     {selected === 1 && postAvehicle()}
+      {selected === 2 && manageVehicles()}
+      {selected === 3 && buyVehicle()}
+      {selected === 4 && sellVehicle()}
+      {selected === 5 && popup()}
+      {selected === 6 && contactUs()}
+      {selected === 7 && finance()}
+    </div>
+  );
+};
+
+export default MainPanel;
+
+const dashboard = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Dashboard
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+const postAvehicle = () => {
+  return (
+    <div>
       <div>
         <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
           Basic Information
@@ -11,6 +40,77 @@ const MainPanel = ({ selected }) => {
       </div>
     </div>
   );
-};
+}
 
-export default MainPanel;
+const manageVehicles = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Manage Vehicles
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+const buyVehicle = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Buy Vehicle
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+const sellVehicle = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Sell Vehicle
+        </h4>
+      </div>
+    </div>
+  );
+} 
+
+const popup = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Popup
+        </h4>
+      </div>
+    </div>
+  );
+} 
+
+const contactUs = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Contact us
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+const finance = () => {
+  return (
+    <div>
+      <div>
+        <h4 className="pb-4 text-xl font-bold uppercase text-truevalue">
+          Finance
+        </h4>
+      </div>
+    </div>
+  );
+}
+
