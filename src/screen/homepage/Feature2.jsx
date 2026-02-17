5cb72a464fdaa8b2ee89de126cb4658ef0b4cc4e
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-02-17 17:48:20 +0530

    updated this

--- src/screen/homepage/Feature2.jsx
+++ src/screen/homepage/Feature2.jsx
@@ -46,90 +46,90 @@ const Feature2 = () => {
 
 export default Feature2;
 
-function LimitedEditonCarSlider() {
-  const navigationPrevRef = useRef(null);
-  const navigationNextRef = useRef(null);
+// function LimitedEditonCarSlider() {
+//   const navigationPrevRef = useRef(null);
+//   const navigationNextRef = useRef(null);
 
-  const { finalData } = useFormData();
+//   const { finalData } = useFormData();
 
-  return (
-    <>
-      <div className="relative overflow-visible select-none ">
-        <Swiper
-          navigation={{
-            nextEl: navigationNextRef.current,
-            prevEl: navigationPrevRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            swiper.params.navigation.prevEl = navigationPrevRef.current;
-            swiper.params.navigation.nextEl = navigationNextRef.current;
-          }}
-          autoplay={{
-            delay: 4000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            dynamicBullets: true,
-          }}
-          loop={true}
-          breakpoints={{
-            100: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 20,
-            },
-            1280: {
-              slidesPerView: 4,
-              spaceBetween: 20,
-            },
-          }}
-          modules={[Pagination, Autoplay, Navigation]}
-          className="container pt-10 pl-10 pr-2 mx-auto overflow-hidden bg-transparent mySwiper "
-        >
-          {/* <div className="h-10"></div> */}
-          <div className="absolute z-10 justify-between  w-full gap-4 flex top-[43%]   ">
-            <div
-              className="flex items-center justify-center w-10 h-10 ml-2 text-white border-2 border-dashed rounded-full cursor-pointer xl:w-12 xl:h-12 hover:bg-primary group border-primary "
-              // onClick={() => setIsTrue(!isTrue)}
-              ref={navigationPrevRef}
-            >
-              <BsArrowLeft className="text-2xl duration-500 translate-x-4 text-primary group-hover:text-white group-hover:translate-x-0 xl:text-3xl" />
-            </div>
+//   return (
+//     <>
+//       <div className="relative overflow-visible select-none ">
+//         <Swiper
+//           navigation={{
+//             nextEl: navigationNextRef.current,
+//             prevEl: navigationPrevRef.current,
+//           }}
+//           onBeforeInit={(swiper) => {
+//             swiper.params.navigation.prevEl = navigationPrevRef.current;
+//             swiper.params.navigation.nextEl = navigationNextRef.current;
+//           }}
+//           autoplay={{
+//             delay: 4000,
+//             disableOnInteraction: false,
+//           }}
+//           pagination={{
+//             dynamicBullets: true,
+//           }}
+//           loop={true}
+//           breakpoints={{
+//             100: {
+//               slidesPerView: 1,
+//               spaceBetween: 10,
+//             },
+//             640: {
+//               slidesPerView: 1,
+//               spaceBetween: 10,
+//             },
+//             768: {
+//               slidesPerView: 2,
+//               spaceBetween: 20,
+//             },
+//             1024: {
+//               slidesPerView: 3,
+//               spaceBetween: 20,
+//             },
+//             1280: {
+//               slidesPerView: 4,
+//               spaceBetween: 20,
+//             },
+//           }}
+//           modules={[Pagination, Autoplay, Navigation]}
+//           className="container pt-10 pl-10 pr-2 mx-auto overflow-hidden bg-transparent mySwiper "
+//         >
+//           {/* <div className="h-10"></div> */}
+//           <div className="absolute z-10 justify-between  w-full gap-4 flex top-[43%]   ">
+//             <div
+//               className="flex items-center justify-center w-10 h-10 ml-2 text-white border-2 border-dashed rounded-full cursor-pointer xl:w-12 xl:h-12 hover:bg-primary group border-primary "
+//               // onClick={() => setIsTrue(!isTrue)}
+//               ref={navigationPrevRef}
+//             >
+//               <BsArrowLeft className="text-2xl duration-500 translate-x-4 text-primary group-hover:text-white group-hover:translate-x-0 xl:text-3xl" />
+//             </div>
 
-            <div
-              className="flex items-center justify-center w-10 h-10 mr-2 text-white border-2 border-dashed rounded-full cursor-pointer xl:w-12 xl:h-12 hover:bg-primary group border-primary"
-              ref={navigationNextRef}
-            >
-              <BsArrowLeft className="text-2xl duration-500 rotate-180 -translate-x-4 text-primary group-hover:translate-x-0 xl:text-3xl group-hover:text-white" />
-            </div>
-          </div>
-          <div className="px-2 overflow-hidden">
-            {finalData.slice(0, 11).map((x, i) => {
-              return (
-                <SwiperSlide key={i} className="relative pb-4 group">
-                  <CarCard2 carData={x} />
-                </SwiperSlide>
-              );
-            })}
-          </div>
-          <div className="h-4"></div>
-        </Swiper>
-      </div>
+//             <div
+//               className="flex items-center justify-center w-10 h-10 mr-2 text-white border-2 border-dashed rounded-full cursor-pointer xl:w-12 xl:h-12 hover:bg-primary group border-primary"
+//               ref={navigationNextRef}
+//             >
+//               <BsArrowLeft className="text-2xl duration-500 rotate-180 -translate-x-4 text-primary group-hover:translate-x-0 xl:text-3xl group-hover:text-white" />
+//             </div>
+//           </div>
+//           <div className="px-2 overflow-hidden">
+//             {finalData.slice(0, 11).map((x, i) => {
+//               return (
+//                 <SwiperSlide key={i} className="relative pb-4 group">
+//                   <CarCard2 carData={x} />
+//                 </SwiperSlide>
+//               );
+//             })}
+//           </div>
+//           <div className="h-4"></div>
+//         </Swiper>
+//       </div>
      
-    </>
-  );
-}
+//     </>
+//   );
+// }
 function LimitedEditonCarSlider2() {
   const navigationPrevRef = useRef(null);
   const navigationNextRef = useRef(null);
